e8d7fe4dd50fd1224756c5252e8d281729a96140
Phil <phil@polarstork.com>
2024-09-20 13:39:34 +0300

    refactor: Update time format in LoggerTimeExtension to include seconds and ms

--- lib/src/extensions/logger_time_extension.dart
+++ lib/src/extensions/logger_time_extension.dart
@@ -2,12 +2,15 @@
 extension LoggerTimeExtension on DateTime {
   /// Returns a human-readable time format.
   ///
-  /// If the date is yesterday, it includes the date in the format `yyyy-MM-dd HH:mm`.
-  /// For all other times, it displays the time in 24-hour format `HH:mm`.
+  /// If the date is yesterday, it includes the date in the format `yyyy-MM-dd HH:mm:ss.SS`.
+  /// For all other times, it displays the time in 24-hour format `HH:mm:ss.SS`.
   String get format {
     final now = DateTime.now();
     final yesterday = now.subtract(const Duration(days: 1));
     final dateToCompare = DateTime(year, month, day);
+
+    final millisecondsTwoDigits =
+        (millisecond ~/ 10).toString().padLeft(2, '0');
 
     if (dateToCompare.year == yesterday.year &&
         dateToCompare.month == yesterday.month &&
@@ -17,10 +20,15 @@ extension LoggerTimeExtension on DateTime {
           '${month.toString().padLeft(2, '0')}-'
           '${day.toString().padLeft(2, '0')} '
           '${hour.toString().padLeft(2, '0')}:'
-          '${minute.toString().padLeft(2, '0')}';
+          '${minute.toString().padLeft(2, '0')}:'
+          '${second.toString().padLeft(2, '0')}.'
+          '$millisecondsTwoDigits';
     }
+
     // For all other times, display time in 24-hour format
     return '${hour.toString().padLeft(2, '0')}:'
-        '${minute.toString().padLeft(2, '0')}';
+        '${minute.toString().padLeft(2, '0')}:'
+        '${second.toString().padLeft(2, '0')}.'
+        '$millisecondsTwoDigits';
   }
 }
